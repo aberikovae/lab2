258d42f578e8f6979943c7c97255e2b7b607390c
aberikovae <158310386+aberikovae@users.noreply.github.com>
2024-01-31 22:21:38 +0600

    Add files via upload

---
+++ Berikovalab2.py
@@ -0,0 +1,91 @@
+#problem1
+print("Hello World")
+
+#problem2
+if 5 > 2:
+    print("YES")
+
+#problem3
+#This is a comment
+
+#problem4
+""""
+This is a comment
+written in 
+more than just one line
+"""
+
+#problem5
+carname = "Volvo"
+
+#problem6
+x = 50 
+
+#problem7
+x = 5
+y = 10
+print(x+y)
+
+#problem8
+x = 5
+y = 10
+z = x+y
+print(z)
+
+#problem9
+x, y, z = "Orange", "Banana", "Cherry"
+
+#problem10
+x = y = z = "Orange"
+
+#problem11
+def myfunc():
+    global x
+    x = "fantastic"
+
+#problem12
+x = 5
+print(type(x))
+int
+
+#problem13
+x = "Hello World"
+print(type(x))
+str
+
+#problem14
+x = 20.5
+print(type(x))
+float
+
+#problem15
+x = ["apple", "banana", "cherry"]
+print(type(x))
+list
+
+#problem16
+x = ("apple", "banana", "cherry")
+print(type(x))
+tuple
+
+#problem17
+x = {"name" : "John", "age" : 36}
+print(type(x))
+dict
+
+#problem18
+x = True
+print(type(x))
+bool
+
+#problem19
+x = 5
+x = float(x)
+
+#problem20
+x = 5.5
+x = int(x)
+
+#problem21
+x = 5
+x = complex(x)
